34791afd40be04c68850a271245862fa5317b2fa
wekouri <eric.wekouri@gmail.com>
2023-03-07 08:58:13 +0100

    feat: redux-dynamic-modules handling and improvements

--- src/components/hocs/NavigationWrapper/NavigationWrapper.tsx
+++ src/components/hocs/NavigationWrapper/NavigationWrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { IModuleStore } from 'redux-dynamic-modules'
+import { DynamicModuleLoader, IModuleStore, IModuleTuple } from 'redux-dynamic-modules'
 import { Persistor } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import { MainState } from 'store/types'
@@ -10,13 +10,20 @@ const NavigationWrapper =
     ReduxScreen: ({ ...props }) => JSX.Element,
     store: IModuleStore<MainState>,
     persistor: Persistor,
+    dynamicReduxModules: IModuleTuple = [],
   ) =>
   (props: { componentId: string; rootTag: number }) => {
+    const children = (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      <DynamicModuleLoader modules={dynamicReduxModules}>
+        <ReduxScreen {...props} />
+      </DynamicModuleLoader>
+    )
+
     return (
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <ReduxScreen {...props} />
-        </PersistGate>
+        <PersistGate persistor={persistor}>{children}</PersistGate>
       </Provider>
     )
   }
